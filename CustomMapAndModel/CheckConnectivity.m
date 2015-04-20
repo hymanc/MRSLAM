bb543367a43b38e6c990171423f5bb23840a2fe4
riboch <rchoroszucha@yahoo.com>
2015-04-19 21:13:36 -0400

    Fixed the connectivity, added a new variable to the mat file: mapEncounters, and I changed CustomData-CompletelyConnected.

--- CustomMapAndModel/CheckConnectivity.m
+++ CustomMapAndModel/CheckConnectivity.m
@@ -1,7 +1,9 @@
-function CheckConnectivity()
+function encounterMAP=CheckConnectivity(data,MAP,PIXDIM,SENSOR)
 
-    load('CustomData.mat')
-    [MAP,PIXDIM]=getTheMAP(THEIMAGE);
+    if (nargin~=4)
+        load('CustomData.mat')
+        [MAP,PIXDIM]=getTheMAP(THEIMAGE);
+    end
     
     encounterMAP=[];
     for a1=1:size(data(1).pose,2)
@@ -9,8 +11,8 @@ function CheckConnectivity()
             r1=customMapMeasurement(data(a2).pose(1,a1),data(a2).pose(2,a1),data(a2).pose(3,a1),MAP,PIXDIM,SENSOR);
             for a3=setdiff(1:numel(data),a2);
                 MAPplusrobot=MAP;
-                rx=round(data(a3).pose(1));
-                ry=round(data(a3).pose(2));
+                rx=round(data(a3).pose(1,a1));
+                ry=round(data(a3).pose(2,a1));
                 MAPplusrobot((rx-2):(rx+2),(ry-2):(ry+2))=1;
                 r2=customMapMeasurement(data(a2).pose(1,a1),data(a2).pose(2,a1),data(a2).pose(3,a1),MAPplusrobot,PIXDIM,SENSOR);
                 if (~(sum(abs(r2-r1))<1E-9))
