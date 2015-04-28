273f54aba348c844db581798f38c404e19894d9f
Cody Hyman <hymanc@umich.edu>
2015-04-28 05:28:28 -0400

    Added acausal join code to other reverse queue updates

--- Clean/script_ReverseQueue_MultiPF_MultiMap.m
+++ Clean/script_ReverseQueue_MultiPF_MultiMap.m
@@ -25,5 +25,35 @@
 %                         plot(odomRevBarDiff(a2,:),'k');
 %                     end
         %revqueue(a1,revcounters(a1))=struct('scan',[],'pose',[],'u',[],'t',[]);
+        
+                % Check for acausal joins
+        enc = revqueue(a1,revcounters(a1)).encounter;
+        if(size(enc,2)>1)
+            % Prior encounter, perform acausal join
+            oldrob=robID(robotInds==enc(1,2));
+            newrob=robID(robotInds==enc(1,3));
+            if(~find(joined==newrob))
+                counters(newrob)=counters(newrob)-1;%t-1.
+                fprintf('Robot %d was invited by Robot %d.\n',enc(1,3),enc(1,2));
+                joined=[joined;newrob];
+                for a2=1:counters(newrob)
+                    revqueue(newrob,a2)=queue(newrob,a2);
+                    queue(newrob,a2)=struct('scan',[],'pose',[],'u',[],'t',[],'encounter',[]);
+                end
+
+                revcounters(newrob)=counters(newrob);
+                counters(newrob)=1;
+
+                relpose=enc(1,4:6);
+                for a2=1:nParticles
+                    robOdom(:,newrob,a2)=robOdom(:,oldrob,a2)+relpose';
+                    robPoseMapFrame(:,t,newrob,a2)=world_to_map_coordinates(robOdom(1:2,newrob,a2), gridSize, offset);
+                    robOdomReverse(:,newrob,a2)=robOdom(:,oldrob,a2)+relpose';
+                    robPoseMapFrameReverse(:,t,newrob,a2)=world_to_map_coordinates(robOdomReverse(1:2,newrob,a2), gridSize, offset);
+                end 
+            end
+        end
+        % End of acausal join
+        
         revcounters(a1)=revcounters(a1)-1;
     end
